1cd42c2115908b9d483be2e161fd23877f53b02c
deejay94 <dahjoyner12@gmail.com>
2017-09-23 11:52:02 -0400

    Add editable method
    Add editable method to ensure that only the user is able to edit their
    items and no one else.

--- app/serializers/favorite_movie_serializer.rb
+++ app/serializers/favorite_movie_serializer.rb
@@ -1,3 +1,7 @@
 class FavoriteMovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :genre, :comment
+  attributes :id, :title, :genre, :comment, :editable
+end
+
+def editable
+  scope == object.user
 end
